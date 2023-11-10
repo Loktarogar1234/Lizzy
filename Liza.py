f5f07faeea2094b0e8bd4ab20ab418218fe841a1
Lizzy <evmixa@gmail.com>
2023-11-10 23:11:06 +0300

    Initial local commit

---
+++ Liza.py
@@ -0,0 +1,11 @@
+print("Введите число а ")
+a = int(input())
+print("Введите число b ")
+b = int(input())
+print("Введите число c ")
+c = int(input())
+#a = 5
+#b = 2
+#c = 8
+n = a*(c-a)+b**2
+print(n) 
